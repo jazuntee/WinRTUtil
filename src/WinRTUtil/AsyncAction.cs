a5e85fc2712c0601cc2c60acc7491f5889eb8f76
Jason Thompson <jasoth@microsoft.com>
2020-06-08 00:07:40 -0400

    Added AsyncAction class

---
+++ src/WinRTUtil/AsyncAction.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using Windows.Foundation;
+
+namespace WinRTUtil
+{
+    public static class AsyncAction
+    {
+        public static Task AsTask(object objAsyncAction)
+        {
+            IAsyncAction asyncAction = objAsyncAction as IAsyncAction;
+            return asyncAction.AsTask();
+        }
+
+        public static void Await(object objAsyncAction)
+        {
+            Task task = AsTask(objAsyncAction);
+            task.Wait();
+        }
+
+        public static void GetResults(object objAsyncAction)
+        {
+            IAsyncAction asyncAction = objAsyncAction as IAsyncAction;
+            asyncAction.GetResults();
+        }
+    }
+    
+}
